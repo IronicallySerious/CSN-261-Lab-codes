4071b3b4e36fb53667cde37b8f4741945143f11b
Twarit <wtwarit@gmail.com>
2018-08-29 11:19:12 +0530

    Post lab on 29th August

--- problems/A4_1.cpp
+++ problems/A4_1.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <list>
 
+#define IN_OUT
+
 struct Data
 {
 	unsigned long data;
@@ -9,9 +11,57 @@ struct Data
 	Data(unsigned long value) : data(value) {}
 };
 
+Data &getItem(std::list<Data> &list, int location)
+{
+	long i = 0;
+	auto temp = list.begin();
+
+	while (i < list.size())
+	{
+		temp++;
+
+		if (i == location)
+		{
+			return temp->data;
+		}
+	}
+}
+
+void AlternatingSplit(std::list<Data> &dataList, IN_OUT std::list<Data> &listA, IN_OUT std::list<Data> &listB)
+{
+	size_t listSize = dataList.size();
+
+	for (size_t item = 0; item < listSize; item++)
+	{
+		listA.push_back(getItem(listA, item));
+		item++;
+
+		if (item == listSize)
+		{
+			break;
+		}
+
+		listB.push_back(getItem(listB, item));
+	}
+}
+
+std::ostream &operator<<(std::ostream &stream, std::list<Data> &dataList)
+{
+	stream << "List: " << std::endl;
+
+	for (auto item : dataList)
+	{
+		stream << item.data << " " << std::endl;
+	}
+
+	return stream;
+}
+
 int main()
 {
 	std::list<Data> dataList;
+	std::list<Data> listA;
+	std::list<Data> listB;
 
 	std::cout << "Enter number of elements" << std::endl;
 	unsigned long N;
@@ -24,9 +74,12 @@ int main()
 		unsigned long temp;
 		std::cin >> temp;
 		dataList.push_back(Data(temp));
+		i++;
 	}
 
-	
+	AlternatingSplit(dataList, listA, listB);
+
+	std::cout << listA << listB << std::endl;
 
 	return 0;
 }
